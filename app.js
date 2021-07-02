a1cb5983f623927cf3ef4bde6b9fa57d243cd2c2
Rashid Ahmad <raashid.ahmad@gmail.com>
2021-07-02 17:46:16 +0500

    Added CORS settings to allow js http requests from the local app

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 
 const app = express();
 const url = 'mongodb://localhost/AddressBook';
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 mongoose.connect(url, {
     useUnifiedTopology: true,
