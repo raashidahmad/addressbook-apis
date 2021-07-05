17e0b87908da96b7efe27d20382b5f347f42332d
Rashid Ahmad <raashid.ahmad@gmail.com>
2021-07-05 23:12:52 +0500

    Added logic for creating jwt tokens on the basis of authentication

--- routing/accountcontroller.js
+++ routing/accountcontroller.js
@@ -6,29 +6,33 @@ const Contact = require('./models/contact');
 
 router.post('/token', async(req, res) => {
     try {
-        const email = req.body.email;
-        const userObj = {};
+        let email = req.body.email;
         if (!email) {
             res.statusCode = 400;
             res.statusMessage = 'Provide an email address';
             const errorObj = { 'token': null, 'message': 'Provide an email address'};
             res.json(errorObj);
         } else {
-            email = 'fake';
-            Contact.find({ 'email': email }).then((contact, error) => {
-                if (contact.length > 0) {
-                    const token = jwt.sign(email, process.env.JWT_ACCESS_TOKEN);
-                    res.json({ 'token': token });
+            Contact.findOne({ 'email': email }, 'name phone email' ,function (error, contact) {
+                if (error) {
+                    res.statusCode = 400;
+                    res.statusMessage = 'Invalid email address provided';
+                    const errorObj = { 'token': null, 'message': 'Invalid email address provided' };
+                    res.json(errorObj);
+                }
+
+                let token = null;
+                if (contact && contact.email) {
+                    token = jwt.sign(contact.email, process.env.JWT_ACCESS_TOKEN);
                 } 
+                res.json({ 'token': token });
             });
-            res.statusCode = 400;
-            res.statusMessage = 'Invalid email address provided';
-            const errorObj = { 'token': null, 'message': 'Invalid email address provided' };
-            res.json(errorObj);
         }
     } catch (err) {
         res.statusCode = 400;
         res.statusMessage = err;
+        const errorObj = { error: err, token: null };
+        res.json(errorObj);
     }
 });
 module.exports = router;
